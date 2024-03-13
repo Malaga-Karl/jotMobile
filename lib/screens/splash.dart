11ddda0ceced73c4ccdd8852aecbeaac95df4af4
Malaga-Karl <karlmalaga06@gmail.com>
2024-03-14 01:26:24 +0800

    changed splash screen to 2s

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -7,7 +7,7 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Timer(const Duration(seconds: 3), () {
+    Timer(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacementNamed('/home');
     });
 
